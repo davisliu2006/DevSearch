a65e5a61f659d8ee9890d6fb83f9ab6ad9c30f60
Davis Liu <davisliu2006@gmail.com>
2025-02-19 00:14:42 -0500

    Add error messages

--- site/app.ts
+++ site/app.ts
@@ -19,60 +19,64 @@ app.get("/index", function(req, res) {
 });
 
 app.get("/search", function(req, res) {
-    let searchQuery = req.query["query"];
-    let searchTags = (typeof(req.query["tags"]) == "string"? req.query["tags"].split(' ') : undefined);
-    let searchData: Array<JobInfo> = [];
-    let categories = tagCategories;
-    if (typeof(searchQuery) == "string") {
-        let queryTokens = searchQuery.split(' ');
-        let read = fs.readFileSync(DIR+"/../data/scrape.json", "utf8");
-        let jobData: JobData = JSON.parse(read);
-        for (let item of jobData.jobsFound) {
-            let title = item.title.toLowerCase();
-            let url = item.url.toLowerCase();
-            let urlRoot = url.split("?")[0];
-            if (searchTags) {
-                let flag = false;
-                for (let tag of searchTags) {
-                    if (item.classification.includes(tag)) {flag = true;}
-                }
-                if (!flag) {continue;}
-            }
-            for (let token of queryTokens) {
-                token = token.toLowerCase();
-                if (token.length >= 2 && token[0] == '"' && token[token.length-1] == '"') {
-                    let innerToken = token.substring(1, token.length-1);
-                    if (lib.split_by_words(urlRoot).includes(innerToken)) {
-                        searchData.push(item);
-                        break;
-                    } else if (lib.split_by_words(title).includes(innerToken)) {
-                        searchData.push(item);
-                        break;
-                    } else if (item.classification.includes(innerToken)) {
-                        searchData.push(item);
-                        break;
+    try {
+        let searchQuery = req.query["query"];
+        let searchTags = (typeof(req.query["tags"]) == "string"? req.query["tags"].split(' ') : undefined);
+        let searchData: Array<JobInfo> = [];
+        let categories = tagCategories;
+        if (typeof(searchQuery) == "string") {
+            let queryTokens = searchQuery.split(' ');
+            let read = fs.readFileSync(DIR+"/../data/scrape.json", "utf8");
+            let jobData: JobData = JSON.parse(read);
+            for (let item of jobData.jobsFound) {
+                let title = item.title.toLowerCase();
+                let url = item.url.toLowerCase();
+                let urlRoot = url.split("?")[0];
+                if (searchTags) {
+                    let flag = false;
+                    for (let tag of searchTags) {
+                        if (item.classification.includes(tag)) {flag = true;}
                     }
-                } else {
-                    if (urlRoot.includes(token)) {
-                        searchData.push(item);
-                        break;
-                    } else if (title.includes(token)) {
-                        searchData.push(item);
-                        break;
-                    } else if (item.classification.includes(token)) {
-                        searchData.push(item);
-                        break;
+                    if (!flag) {continue;}
+                }
+                for (let token of queryTokens) {
+                    token = token.toLowerCase();
+                    if (token.length >= 2 && token[0] == '"' && token[token.length-1] == '"') {
+                        let innerToken = token.substring(1, token.length-1);
+                        if (lib.split_by_words(urlRoot).includes(innerToken)) {
+                            searchData.push(item);
+                            break;
+                        } else if (lib.split_by_words(title).includes(innerToken)) {
+                            searchData.push(item);
+                            break;
+                        } else if (item.classification.includes(innerToken)) {
+                            searchData.push(item);
+                            break;
+                        }
+                    } else {
+                        if (urlRoot.includes(token)) {
+                            searchData.push(item);
+                            break;
+                        } else if (title.includes(token)) {
+                            searchData.push(item);
+                            break;
+                        } else if (item.classification.includes(token)) {
+                            searchData.push(item);
+                            break;
+                        }
                     }
                 }
             }
+        } else {
+            searchQuery = "";
         }
-    } else {
-        searchQuery = "";
+        if (searchData.length > 100) {
+            searchData.length = 100;
+        }
+        res.render("search.ejs", {searchQuery, searchTags, searchData, categories});
+    } catch {
+        res.render("500.ejs");
     }
-    if (searchData.length > 100) {
-        searchData.length = 100;
-    }
-    res.render("search.ejs", {searchQuery, searchTags, searchData, categories});
 });
 
 app.get("/about", function(req, res) {
